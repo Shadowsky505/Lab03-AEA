ee0b3fbb48382e6f45a18003975073e11e5bbf44
Ayrtoon Tintaya <ayrtoon.tintaya@tecsup.edu.pe>
2024-11-15 06:43:04 -0500

    Subiendo

---
+++ iniciador.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+apt update
+
+apt install docker.io docker-compose
+
+echo "Construyendo la imagen..."
+docker-compose build
+echo "Iniciando el contenedor..."
+docker-compose up -d
+
+#!/bin/bash
+echo "Uso de recursos del contenedor:"
+docker stats --no-stream --format "table {{.Name}}\t{{.MemUsage}}\t{{.BlockIO}}"
+
+echo "RAM del host:"
+free -h
